1017bbb37933235e122d2207be1e0fdbac9777b8
MariiaIog <Suhaya_me@mail.ru>
2022-11-22 03:25:23 +0300

    задача 6

---
+++ task3/Program.cs
@@ -0,0 +1,7 @@
+﻿/* Задача 6: напишите программу, которая на вход принимает число и выдает,
+является ли число четным.*/
+
+int Num = new int();
+Console.WriteLine("Введите число");
+Num = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine(Num % 2 == 0);
